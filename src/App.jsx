00e13e8b14ae2d1415f8b92ef810ecfa4b639b02
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-11-05 16:34:52 +0200

    Fixing missing paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  const [count, setCount] = useState(0);
-
-    return <div>
-        <h2>My first react exercise</h2>
-    </div>;
+  return (
+    <div>
+      <h2>My first react exercise</h2>
+    </div>
+  );
 }
 
 export default App;
