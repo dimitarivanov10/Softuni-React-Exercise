b93e9808b646f8eab46bb9143ac424282d7b4c42
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-11-10 11:15:09 +0200

    Dividing the pricing plans as separate components

--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -1,3 +1,5 @@
+import PricingItem from "./PricingItem";
+
 export default function PricingPlans() {
   return (
     <section className="section colored" id="pricing-plans">
@@ -20,102 +22,31 @@ export default function PricingPlans() {
         </div>
         {/* ***** Section Title End ***** */}
         <div className="row">
-          {/* ***** Pricing Item Start ***** */}
-          <div
-            className="col-lg-4 col-md-6 col-sm-12"
-            data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s"
-          >
-            <div className="pricing-item">
-              <div className="pricing-header">
-                <h3 className="pricing-title">Starter</h3>
-              </div>
-              <div className="pricing-body">
-                <div className="price-wrapper">
-                  <span className="currency">$</span>
-                  <span className="price">14.50</span>
-                  <span className="period">monthly</span>
-                </div>
-                <ul className="list">
-                  <li className="active">60 GB space</li>
-                  <li className="active">600 GB transfer</li>
-                  <li className="active">Pro Design Panel</li>
-                  <li>15-minute support</li>
-                  <li>Unlimited Emails</li>
-                  <li>24/7 Security</li>
-                </ul>
-              </div>
-              <div className="pricing-footer">
-                <a href="#" className="main-button">
-                  Purchase Now
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* ***** Pricing Item End ***** */}
-          {/* ***** Pricing Item Start ***** */}
-          <div
-            className="col-lg-4 col-md-6 col-sm-12"
-            data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s"
-          >
-            <div className="pricing-item active">
-              <div className="pricing-header">
-                <h3 className="pricing-title">Premium</h3>
-              </div>
-              <div className="pricing-body">
-                <div className="price-wrapper">
-                  <span className="currency">$</span>
-                  <span className="price">21.50</span>
-                  <span className="period">monthly</span>
-                </div>
-                <ul className="list">
-                  <li className="active">120 GB space</li>
-                  <li className="active">1200 GB transfer</li>
-                  <li className="active">Pro Design Panel</li>
-                  <li className="active">15-minute support</li>
-                  <li>Unlimited Emails</li>
-                  <li>24/7 Security</li>
-                </ul>
-              </div>
-              <div className="pricing-footer">
-                <a href="#" className="main-button">
-                  Purchase Now
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* ***** Pricing Item End ***** */}
-          {/* ***** Pricing Item Start ***** */}
-          <div
-            className="col-lg-4 col-md-6 col-sm-12"
-            data-scroll-reveal="enter bottom move 50px over 0.6s after 0.6s"
-          >
-            <div className="pricing-item">
-              <div className="pricing-header">
-                <h3 className="pricing-title">Advanced</h3>
-              </div>
-              <div className="pricing-body">
-                <div className="price-wrapper">
-                  <span className="currency">$</span>
-                  <span className="price">42.00</span>
-                  <span className="period">monthly</span>
-                </div>
-                <ul className="list">
-                  <li className="active">250 GB space</li>
-                  <li className="active">5000 GB transfer</li>
-                  <li className="active">Pro Design Panel</li>
-                  <li className="active">15-minute support</li>
-                  <li className="active">Unlimited Emails</li>
-                  <li className="active">24/7 Security</li>
-                </ul>
-              </div>
-              <div className="pricing-footer">
-                <a href="#" className="main-button">
-                  Purchase Now
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* ***** Pricing Item End ***** */}
+          <PricingItem
+            title="Starter"
+            price={14.5}
+            space={60}
+            transfer={600}
+            pro
+          />
+          <PricingItem
+            title="Premium"
+            price={21.5}
+            space={120}
+            transfer={1200}
+            pro
+            active
+          />
+          <PricingItem
+            title="Advanced"
+            price={42.0}
+            space={250}
+            transfer={5000}
+            pro
+            support
+            unlimitedEmails
+            security
+          />
         </div>
       </div>
     </section>
